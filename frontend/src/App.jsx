eece29183291412235dd72186dfbf895561ecdd4
Aneesh004 <111515816+Aneesh004@users.noreply.github.com>
2024-08-20 19:58:23 +0530

    Normalize line endings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import Home from './home/Home';
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom";
 import Courses from './courses/Courses';
-import Signup from './components/Signup';
 import Contact from './components/Contact';
-
+import Signup from './components/Signup';
+import Login from './components/Login';
+import { Toaster } from 'react-hot-toast';
+import { UseAuth } from './context/AuthProvider';
 
 function App() {
+  const [authUser, setAuthUser] = UseAuth()
+  console.log(authUser);
   return (
     <>
-    <div className='dark:bg-slate-900 dark:text-white'>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/course" element={<Courses/>} />
-      <Route path="/signup" element={<Signup/>} />
-      <Route path="/contact" element={<Contact/>} />
-      </Routes>  
-    </div>
+      <div className='dark:bg-slate-900 dark:text-white'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/course" element={authUser ? <Courses /> : <Navigate to="/signup" />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Toaster />
+      </div>
     </>
   );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Login from './Login';
+import Logout from './Logout';
+import { UseAuth } from '../context/AuthProvider';
 
 function Navbar() {
+    const [authUser,setAuthUser]=UseAuth();
     const [theme, setTheme] = useState(
         localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
     );
@@ -101,7 +104,7 @@ function Navbar() {
                             <div className="hidden md:block">
                                 <label className="px-3 py-2 border rounded-md flex items-stretch gap-2 " style={{ border: '1px solid black' }}>
                                     <input type="text"
-                                        className=" grow outline-none bg-transparent text-black grow dark:text-white space-normal"
+                                        className=" grow outline-none bg-transparent text-black dark:text-white space-normal"
                                         placeholder="Explore" />
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
@@ -140,15 +143,22 @@ function Navbar() {
                                         d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
                                 </svg>
                             </label>
+
+                            {
+                                authUser ? (
+                                <Logout/>
+                            ):(
                             <div className="">
                                 <a
-                                    className="bg-purple-600 text-white px-3 py-2 rounded-md hover:bg-blue-900 duration-300 cursor-pointer"
-                                    onClick={() => document.getElementById("my_modal_3").showModal()}
+                                    className="bg-purple-600 text-white px-3 py-2 rounded-md hover:bg-purple-700 duration-300 cursor-pointer"
+                                    onClick={() => document.getElementById("my_modal_3").showModal()
+                                    }
                                 >
                                     Login
                                 </a>
                                 <Login />
                             </div>
+                            )}
 
                         </div>
                     </div>
